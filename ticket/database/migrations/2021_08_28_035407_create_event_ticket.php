3b2d74d5ef2ccfce4373987e32c5acb247cf51a2
Sacalpizar <sacalpizar@gmail.com>
2021-07-14 13:25:37 -0600

    Avance04

---
+++ ticket/database/migrations/2021_08_28_035407_create_event_ticket.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventTicket extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_evento');
+            $table->string('precio');
+            $table->integer('cantidad');
+            $table->foreignId('categories_id')->constrained();
+            $table->string('imagen');
+            $table->string('descripcion');
+            $table->string('lugar');
+            $table->date('fecha');
+            $table->time('hora');
+            $table->string('tipo');
+            $table->string('publico');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
